812444db7a1de9288b2cca2c0a4fa4fe1b93b33f
Kai <129865741+kai3pi@users.noreply.github.com>
2024-05-17 21:17:14 -0400

    cal_square_root5.py
    This commit adds the initial version of the cal_square_root5.py script, which calculates Square root of 5 to a specified number of digits using the Newton's method. This script is intended to provide an educational example of mathematical computations in Python.

--- Square_root5_calculator/cal_square_root5.py
+++ Square_root5_calculator/cal_square_root5.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         digits = int(sys.argv[1])  #Get number of digits from command line
     else:
-        digits = 10  #Default value when there are no command line arguments
+        digits = 10   #Default value when there are no command line arguments
 
     root5 = sqrt_5(digits)
     print(str(root5))
